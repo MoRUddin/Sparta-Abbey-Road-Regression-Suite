ab8e8abcae6a4e5df1f1ca51b3ee1088dcd0c150
Tech-A22 <tech-a22@Tech-A22s-MacBook-Air.local>
2018-04-10 16:00:55 +0100

    live feed feature and step definitions skeleton setup

---
+++ features/step_definitions/live_feed_step_defs.rb
@@ -0,0 +1,103 @@
+Before do
+
+end
+
+Given("I am on Google's search page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I search "Abbey Road Crossing"') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I search "Abbey Road Studios"') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the link Visit Abbey Road Studios") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am redirected to the Live Feed Page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the Live Feed Page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click one of the time tabs") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the feed plays from that selected point") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on Abbey Road's homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click "Visit Us"') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click "Abbey Road Studios: The Most Famous Recording Studios"') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I am redirected to the Homepage") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("that tab turn red") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("it should be defaulted to the live feed") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can see the 6 images on the Wall of Fame") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the images are in chronological order") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click the share button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a modal appears to share the image") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I am on the sharing modal") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click the email button") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I fill in my name") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I fill in the email from") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I fill in the email to") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click send") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I receive an email") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
