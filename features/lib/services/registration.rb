2dd55bd76c754cf6e30962c0387eaf7d847b514a
Mo <rayhaan_spin@hotmail.com>
2018-04-13 10:59:44 +0100

    indentation cleanup

--- features/lib/services/registration.rb
+++ features/lib/services/registration.rb
@@ -2,20 +2,9 @@ class RegisterPage
   include Capybara::DSL
 
   REGISTER_URL = "https://stagemy.abbeyroad.com/account/login"
-  # REGISTER_NAME_FIELD_XPATH = '//*[@id="register"]/div/form/div[1]/div/input'
-  # REGISTER_EMAIL_FIELD_XPATH = '//*[@id="register"]/div/form/div[1]/div/input'
-  # REGISTER_EMAIL_CONFIRM_FIELD_XPATH = '//*[@id="register"]/div/form/div[1]/div/input'
-  # REGISTER_PASSWORD_FIELD_XPATH = '//*[@id="register"]/div/form/div[4]/div/input'
-  # REGISTER_PASSWORD_CONFIRMATION_FIELD_XPATH = '//*[@id="register"]/div/form/div[5]/div/input'
-
-<<<<<<< HEAD
   REGISTER_SCOPE = '//*[@id="register"]'
   LOGIN_SCOPE = '//*[@id="login"]'
-=======
-REGISTER_SCOPE = '//*[@id="register"]'
-LOGIN_SCOPE = '//*[@id="login"]'
-USERNAME_ID = 'name'
->>>>>>> dev
+  USERNAME_ID = 'name'
 
   def get_url
     current_url
@@ -38,24 +27,13 @@ USERNAME_ID = 'name'
       fill_in 'Password', :with => password
     end
   end
-<<<<<<< HEAD
-  def click_register_button button_text
-    click_button(button_text)
-  end
-=======
-
   def click_button_by_text button_text
     click_button(button_text)
   end
-
   def get_url
     current_url
   end
-
   def click_username_link
     click_button(USERNAME_ID)
   end
-
-
->>>>>>> dev
 end
