ff20f7bc058c83ec02c77f09247894602fd23c78
cbraynt93 <cbryant1993@gmail.com>
2018-04-17 12:20:04 +0100

    scenario for studio 1,2,3, mixing and mastering complete

--- features/lib/services/contact_us.rb
+++ features/lib/services/contact_us.rb
@@ -2,5 +2,24 @@
 class ContactUs
   include Capybara::DSL
 
+  def visit_url(url)
+    visit(url)
+  end
+
+  def read_contact_picture
+    find("div.details > img").visible?
+  end
+
+  def read_contact_name
+    find("div.details > h3").visible?
+  end
+
+  def read_contact_title
+    find("div.details > p").visible?
+  end
+
+  def read_contact_number
+    find("div.details > ul > li ").visible?
+  end
 
 end
