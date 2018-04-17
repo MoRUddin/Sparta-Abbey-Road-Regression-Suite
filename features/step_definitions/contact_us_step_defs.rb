c0ef9e73ae7d57e4a1ea432c49a3cf4238dd63eb
Mo <rayhaan_spin@hotmail.com>
2018-04-17 13:49:06 +0100

    merge conflicts tidy up

--- features/step_definitions/contact_us_step_defs.rb
+++ features/step_definitions/contact_us_step_defs.rb
@@ -29,3 +29,35 @@ end
 Then("I can see it points to Abbey Road Studios address by default") do
   expect(@contact_us.check_default_location).to include "Abbey%20Road%20Studios"
 end
+
+Given("I am on the studio 2 page") do
+  @contact_us.visit_url("https://stage.abbeyroad.com/studio-two")
+end
+
+Given("I am on the studio 3 page") do
+  @contact_us.visit_url("https://stage.abbeyroad.com/studio-three")
+end
+
+Given("I am on the Recording & Mixing page") do
+  @contact_us.visit_url("https://stage.abbeyroad.com/recording-mixing")
+end
+
+Given("I am on the Mastering page") do
+  @contact_us.visit_url("https://stage.abbeyroad.com/mastering")
+end
+
+Then("I can see the point of contact's picture") do
+  expect(@contact_us.read_contact_picture).to be true
+end
+
+Then("Their name") do
+  expect(@contact_us.read_contact_name).to be true
+end
+
+Then("Their job title") do
+  expect(@contact_us.read_contact_title).to be true
+end
+
+Then("Their phone number") do
+  expect(@contact_us.read_contact_number).to be true
+end
